347c3d2f11f065b0e1d158b132b01b56f990555a
dwi sulfahnur <dwisulfahnur@gmail.com>
2016-10-24 22:39:30 +0800

    Append Register Request

---
+++ app/Http/Requests/RegisterRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RegisterRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'image'             =>  'required|image|mimes:jpeg,png,jpg,gif,svg|max:2024',
+            'full_name'         =>  'required|min:8|max:50',
+            'email'             =>  'required|unique:users,email|min:8|email',
+            'username'          =>  'required|unique:users,username|min:8',
+            'password'          =>  'required|min:8|max:50',
+        ];
+    }
+}
