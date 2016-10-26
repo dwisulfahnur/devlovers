b0efc6491b2dccbae79508e8d51b81736af83e53
dwi sulfahnur <dwisulfahnur@gmail.com>
2016-10-26 15:18:55 +0800

    append layout

---
+++ resources/views/user/layouts/app.blade.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <title>App Name - @yield('title')</title>
+        <link rel="stylesheet" href="css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    </head>
+    <body>
+
+        <div class="container">
+            @yield('content')
+        </div>
+
+        <!-- Latest compiled and minified JavaScript -->
+        <script src="js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    </body>
+</html>
