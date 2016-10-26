d49b5ccbb64ce1d22af60d141f02168db90c2533
dwi sulfahnur <dwisulfahnur@gmail.com>
2016-10-26 22:01:55 +0800

    Assign middleware for routes

--- routes/web.php
+++ routes/web.php
@@ -12,11 +12,19 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('home');
+})->name('welocome');
+
+Route::group(['middleware' => 'checkuser'], function(){
+    Route::get('/home', 'UserController@home')->name('home');
 });
 
-Route::get('/register', 'UserController@register')->name('register');
-Route::post('/register', 'UserController@postRegister')->name('postRegister');
+Route::group(['middleware' => 'foruser'], function(){
+    Route::get('/register', 'UserController@register')->name('register');
+    Route::post('/register', 'UserController@postRegister');
 
-Route::get('/login', 'UserController@login')->name('register');
-Route::post('/login', 'UserController@login')->name('postLogin');
+    Route::get('/login', 'UserController@login')->name('login');
+    Route::post('/login', 'UserController@postlogin');
+});
+
+Route::get('/logout', 'UserController@logout')->name('logout');
