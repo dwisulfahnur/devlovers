5dd4681adae6414166d65231c4b188cabfa1aa8c
dwisulfahnur <dwisulfahnur@gmail.com>
2016-11-07 21:05:10 +0800

    modified and append validation for PUT and POST method

--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Foundation\Http\FormRequest;
 
 class UserRequest extends FormRequest
@@ -23,21 +24,33 @@ class UserRequest extends FormRequest
      */
     public function rules()
     {
-        switch($this->method()){
-            case 'GET':
-            case 'POST':
-            {
-                return [
-                    'image'                 =>  'required|image|mimes:jpeg,png,jpg,gif,svg|max:2024',
-                    'full_name'             =>  'required|min:8|max:50',
-                    'email'                 =>  'required|unique:users,email|min:8|email',
-                    'username'              =>  'required|unique:users,username|min:8',
-                    'password'              =>  'required|min:6|confirmed',
-                    'password_confirmation' =>  'required|min:6|same:password',
-                    'dob'                   =>  'required|before:now',
-                ];
-            }
-            default: break;
+        if($this->isMethod('post')){
+            return [
+                'image'                 =>  'required|image|mimes:jpeg,png,jpg,gif,svg|max:2024',
+                'full_name'             =>  'required|min:8|max:50',
+                'email'                 =>  'required|min:8|email|unique:users,email',
+                'username'              =>  'required|min:8|unique:users,username',
+                'password'              =>  'required|min:6|confirmed',
+                'password_confirmation' =>  'required|min:6|same:password',
+                'dob'                   =>  'required|before:now',
+                'gender'                =>  'required|boolean',
+                'roles'                 =>  'required|exists:cities,id',
+                'city'                  =>  'required|exists:cities,id'
+            ];
+        }
+
+        if($this->isMethod('put')){
+            return [
+                'image'                 =>  'image|mimes:jpeg,png,jpg,gif,svg|max:2024',
+                'full_name'             =>  'required|min:8|max:50',
+                'email'                 =>  'required|min:8|email|unique:users,email,'.$this->session()->get('id'),
+                'username'              =>  'required|min:8|unique:users,username,'.$this->session()->get('id'),
+                'dob'                   =>  'required|before:now',
+                'gender'                =>  'required|boolean',
+                'roles'                 =>  'required|exists:roles,id',
+                'city'                  =>  'required|exists:cities,id'
+            ];
+
         }
     }
 }
