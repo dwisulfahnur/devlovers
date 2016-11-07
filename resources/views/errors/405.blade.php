aae6564a14f3ad37a625379527a463b2a36ed7e9
dwisulfahnur <dwisulfahnur@gmail.com>
2016-11-07 21:01:07 +0800

    Append errors page

---
+++ resources/views/errors/405.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('title', '404 - Page Not Found')
+
+    @section('content')
+        <div class="col-md-12" style="text-align:center; font-size:80; margin: 150 auto">
+            Method Not Allowed
+        </div>
+    @endsection
