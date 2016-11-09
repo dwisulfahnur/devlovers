b5b3bb9468c4b828effc6be86dfb7d2f28c44170
dwisulfahnur <dwisulfahnur@gmail.com>
2016-11-09 15:24:46 +0800

    Add Input and Image class to app.php

--- config/app.php
+++ config/app.php
@@ -163,7 +163,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         Collective\Html\HtmlServiceProvider::class,
-
+        Intervention\Image\ImageServiceProvider::class,
 
         /*
          * Package Service Providers...
@@ -180,10 +180,6 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
-        /*
-         * Application Debugbar Providers...
-         */
-         //Barryvdh\Debugbar\ServiceProvider::class,
 
     ],
 
@@ -234,7 +230,8 @@ return [
         'View' => Illuminate\Support\Facades\View::class,
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
+        'Image' => Intervention\Image\Facades\Image::class,
+        'Input' => Illuminate\Support\Facades\Input::class,
 
-        //'Debugbar' => Barryvdh\Debugbar\Facade::class,
     ],
 ];
